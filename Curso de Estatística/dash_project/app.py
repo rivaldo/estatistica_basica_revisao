5d1cfad96790ff23dd08e1f767cd391b8af2ae0e
Rivaldo da Silva Junior <rivaldoj@see.pb>
2023-11-13 15:50:27 -0300

    sexto commit

--- Curso de Estatística/dash_project/app.py
+++ Curso de Estatística/dash_project/app.py
@@ -1,11 +1,39 @@
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(
     [
-        html.H1('Hello World!')
+        html.H1(
+            children='Estudo do Censo',
+            style={
+                'color': 'blue',
+                'fontsize': '40px'
+            }
+        ),
+        html.H2('INEP - CENSO ESCOLAR - MICRODADOS'),
+        html.P('Dados interessantes'),
+        dbc.Tabs([
+            dbc.Tab([
+                html.Ul([
+                    html.Li('Número de escolas estaduais em 2022'),
+                    html.Li('Período de tempo abordado: 2007 a 2022'),
+                    html.Li('Frequência que é realizado o censo: ANUAL'),
+                    html.Li('Último censo realizado: 2022'),
+                    html.Li([
+                        'Fonte dos dados: ',
+                        html.A('Censo Escolar - Microdados do Censo Escolar da Educacação Básica', 
+                        href='https://www.gov.br/inep/pt-br/acesso-a-informacao/dados-abertos/microdados/censo-escolar'
+                        )
+            ])
+            
+        ])
+            ] , label='Informaçoes do Projeto')
+        ]),
+        
+        
     ]
 )
 
